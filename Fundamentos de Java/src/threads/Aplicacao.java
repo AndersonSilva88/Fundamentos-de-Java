e08e5d4ef9db0794c35e51a61a448a6dd14e709f
perkles <anderson.os88@hotmail.com>
2021-08-01 11:28:53 -0300

    threads

---
+++ Fundamentos de Java/src/threads/Aplicacao.java
@@ -0,0 +1,22 @@
+package threads;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Contador c = new Contador();
+		c.start();
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+
+	}
+
+}
